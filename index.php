db4d0fa5ffb426440e7d12e36badd17e8288e485
rusZovnet3 <cferls_ru@hotmail.com>
2023-08-09 17:17:16 -0400

    Prueba 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP</title>
+</head>
+<body>
+	<h1>Probando</h1>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio voluptatum voluptates, consequuntur, unde praesentium cum eum!</p>
+</body>
+</html>
